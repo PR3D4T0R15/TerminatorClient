205bf38228da132956724f82c0bdc1c59aec153d
Matt <32712980+PR3D4T0R15@users.noreply.github.com>
2023-01-23 01:54:03 +0100

    Okno główne układ

--- Terminator_client/src/mainWindow/mainwindow.cpp
+++ Terminator_client/src/mainWindow/mainwindow.cpp
@@ -9,11 +9,39 @@ mainWindow::mainWindow(QWidget *parent) :
     ui(new Ui::mainWindow)
 {
     ui->setupUi(this);
+
+    ui->tableWidget_lists->horizontalHeader()->hide();
+    ui->tableWidget_lists->setColumnWidth(0, 50);
+    ui->tableWidget_lists->horizontalHeader()->setStretchLastSection(true);
+
+
+    ui->tableWidget_tasks->horizontalHeader()->hide();
+    ui->tableWidget_tasks->setColumnWidth(0, 50);
+    ui->tableWidget_tasks->setColumnWidth(0, 50);
+    ui->tableWidget_tasks->horizontalHeader()->setStretchLastSection(true);
+
+    QPixmap add(":/Terminator_client/src/img/add.png");
+    QPixmap remove(": / Terminator_client / src / img / remove.png");
+    QPixmap send(":/Terminator_client/src/img/send.png");
+
+    QIcon IcoAdd(add);
+    QIcon IcoRemove(remove);
+    QIcon IcoSend(send);
+
+    ui->pushButton_sendList->setIcon(IcoSend);
+    ui->pushButton_sendList->setIconSize(QSize(20, 20));
+
+    ui->pushButton_addLists->setIcon(IcoAdd);
+    ui->pushButton_addLists->setIconSize(QSize(20, 20));
+
+    ui->pushButton_addLists->setIcon(IcoAdd);
+    ui->pushButton_addLists->setIconSize(QSize(20, 20));
 }
 
 mainWindow::~mainWindow()
 {
     delete ui;
+    
 }
 
 void mainWindow::ShowWindow(QString username, QString password)
